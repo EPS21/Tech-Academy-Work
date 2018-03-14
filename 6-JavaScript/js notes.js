d99f8c1bc50a916ccdd2f5b2f414c8af07611ef8
Eric Sheng <ericsheng21@gmail.com>
2018-03-13 18:21:17 -0700

    add vs folder

--- 6-JavaScript/js notes.js
+++ 6-JavaScript/js notes.js
@@ -2,11 +2,24 @@
 
 num1 = 1;
 num2 = 2;
-numberArray = [2,3,6,7,11,4];
+numberArray = [2,3,"c",7,11,4];
+numberArray[2] = "f";
+numberArray.push("x");
 
 for (var i = 0; i < numberArray.length; i++) {
 	console.log(numberArray[i]);
-
 	
 }
+var num = 6;
+result = (num >= 4);
+//console.log(result);
 
+function myFunction() {	
+	console.log(typeof result);
+
+}
+myFunction();
+
+var str = "Hello World!";
+var result = str.fontcolor("green");
+console.log(result);
