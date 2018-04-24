b4ba90aff7d839b8b7289dc0f41055c56273d239
Eric Sheng <ericsheng21@gmail.com>
2018-04-24 01:39:35 -0700

    adding mvc

---
+++ 8-cSharp/VS_MVC_repos/HelloMVC/HelloMVC/Models/HelloModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloMVC.Models
+{
+    public class HelloModel
+    {
+        public static List<Car> GetCars()
+        {
+            List<Car> cars = new List<Car>
+            {
+                new Car { Id = 1, Make = "Mazda", Model = "6"},
+                new Car { Id = 2, Make = "Toyota", Model = "Hachi Roku"},
+                new Car { Id = 3, Make = "BMW", Model = "528i"}
+            };
+            return cars;
+        }
+    }
+
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+
+}
