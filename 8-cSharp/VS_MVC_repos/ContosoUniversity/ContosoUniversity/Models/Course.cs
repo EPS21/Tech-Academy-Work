6e5df0b633727927471ab6f9eeca910b826a1eeb
Eric Sheng <ericsheng21@gmail.com>
2018-04-27 00:03:39 -0700

    add contoso univ project first half

---
+++ 8-cSharp/VS_MVC_repos/ContosoUniversity/ContosoUniversity/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)] // this lets you enter primary key for course (instead of DB generating it)
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        // navigation property
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ 8-cSharp/VS_MVC_repos/ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,19 @@
+﻿namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        // primary key
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; } // Grade? means its nullable
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
